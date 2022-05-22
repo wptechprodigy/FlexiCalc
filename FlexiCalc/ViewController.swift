bfac79d94f64a8216cfc6b523f364282fb58bfb1
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-05-22 06:06:03 +0100

    Initial Commit

---
+++ FlexiCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlexiCalc
+//
+//  Created by waheedCodes on 22/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
